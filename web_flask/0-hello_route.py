9bbdeb144ff6c11d0197700203c93c41646b1f44
attamakevin <kevinejikez@gmail.com>
2023-05-20 15:29:44 -0700

    fizes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,14 @@
 """A script that starts a flask web application
    listening on 0.0.0.0, port 5000
 """
-from web_flask.__init__ import app
-@app.route('/', strict_slashes=False)
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route('/', strict_slashes=False)
 def hello():
     return ("Hello HBNB!")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,13 @@ app = Flask(__name__)
 def hello():
     """Return a given string"""
     return ("Hello HBNB!")
+
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Returns a given string"""
     return ("HBNB")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
